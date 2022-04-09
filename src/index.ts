7c4ae74cb90b861bd66bef982e26c2ee3f04039f
NehuenLom <93491147+Nehuenlom@users.noreply.github.com>
2022-04-09 16:58:55 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//let elementoInput = document.getElementById("input-nombre");
+//let elementoBoton = document.getElementById("botonSaludo");
+
+//function mostrarNombreIngresado() {
+//let valorInput = elementoInput.value;
+//alert("Saludos" valorInput);
+//console.log("Hola" valorInput);
+//}
+//elementoBoton?.addEventListener("click", mostrarNombreIngresado);
+let mostrarNombreIngresado : Text = prompt("Ingresar Nombre");
+console.log("Hola"mostrarNombreIngresado);
